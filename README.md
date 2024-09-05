2363e0e8c70dd746061ac18a8733172de0b76f94
shankerk <shankerk.in@mouritech.com>
2024-09-05 12:06:33 +0530

    sign-up-form completed

--- README.md
+++ README.md
@@ -25,3 +25,6 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+
+remember when ever you adding bootstrap add at the index.html link url and then in the angular.json path of the bootstrap in node modules
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+function customPasswordValidator(control: FormControl): { [key: string]: boolean } | null {
+  // checking if the password contains at least one uppercase letter
+  const value: string = control.value;
+  if (!/[A-Z]/.test(value)) {
+    // Return an object with a key-value pair indicating validation failure
+    return { 'uppercase': true };
+  }
+  return null;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-reactive-forms-example';
+  loginForm!: FormGroup;
+  constructor(){
+
+  }
+  ngOnInit(){
+    this.loginForm=new FormGroup({
+      email:new FormControl('',[Validators.required,Validators.email]),
+      password: new FormControl('',[Validators.required,Validators.minLength(7),customPasswordValidator])
+    })
+    console.log(this.loginForm.get('password')?.errors)
+  }
 }
